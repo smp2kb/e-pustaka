10332642233dd675f53e9d1b12b335d9c1bbe4a7
smp2kb <98651668+smp2kb@users.noreply.github.com>
2022-01-29 22:14:25 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//------------------------------::::::::::::::::::::------------------------------\\
-// Dibuat oleh di PT. Pacifica Raya Technology \\
-//------------------------------::::::::::::::::::::------------------------------\\
-
-// Arahkan user ketika pertama kali membuka applikasi ke halaman login
-header("location: masuk");
